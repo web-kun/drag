46f69cbd6464d5fd22f38b06c8da3ec7dcddc941
web-kun <fengzekun_web@163.com>
2016-01-19 17:22:53 +0800

    change1 draw.js

--- draw.js
+++ draw.js
@@ -2,8 +2,9 @@
 * @Author: fengzekun
 * @Date:   2016-01-19 16:34:32
 * @Last Modified by:   fengzekun
-* @Last Modified time: 2016-01-19 17:17:50
+* @Last Modified time: 2016-01-19 17:22:38
 */
 
-'use strict';
-var a = 1;
+function draw(){
+	
+}
