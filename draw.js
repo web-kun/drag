08b6ad43c0281622ee62aa4c5744d64eb5ad7aca
web-kun <fengzekun_web@163.com>
2016-01-20 15:38:30 +0800

    aa

--- draw.js
+++ draw.js
@@ -2,12 +2,14 @@
 * @Author: fengzekun
 * @Date:   2016-01-19 16:34:32
 * @Last Modified by:   fengzekun
-* @Last Modified time: 2016-01-19 17:24:49
+* @Last Modified time: 2016-01-19 17:32:46
 */
 
 function draw( id ){
 	var elm = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
 	elm.onmousedown = function(){
-		
+
 	}
 }
