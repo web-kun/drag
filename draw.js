8387b5470645c383ed9cf3ebe959cf2cab67bd6e
web-kun <fengzekun_web@163.com>
2016-01-20 15:46:20 +0800

    change3 draw.js and demo1.html

--- draw.js
+++ draw.js
@@ -2,13 +2,14 @@
 * @Author: fengzekun
 * @Date:   2016-01-19 16:34:32
 * @Last Modified by:   fengzekun
-* @Last Modified time: 2016-01-19 17:32:46
+* @Last Modified time: 2016-01-20 15:44:48
 */
 
 function draw( id ){
 	var elm = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
+	
 	elm.onmousedown = function(){
 
 	}
